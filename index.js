238c835dde37d12567b4359f31e521474f5dbee2
Unknown <mingkaichen2009@gmail.com>
2017-10-07 14:09:53 -0700

    added and extended db api from uas

---
+++ index.js
@@ -0,0 +1,117 @@
+const mongoose = require('mongoose');
+const grid = require('gridfs-stream');
+
+require('./connect_mongo');
+
+var AudioSchema = require('./_schemas/audio_schema');
+
+var AudioModel = require('./_models/audio_model');
+var PopularModel = require('./_models/popular_model');
+
+var gfs = null;
+var connection = mongoose.connection;
+connection.once('connected', () => {
+	gfs = grid(connection.db, mongoose.mongo);
+});
+
+// >>>> AUDIO MODEL <<<<
+// ACCESSOR
+exports.audioQuery = (query, limit) => { // get metadata
+	var allQuery = AudioModel.find(query);
+	allQuery = allQuery.limit(limit);
+	return allQuery.exec();
+};
+
+exports.getAudio = (id) => { // get audio stream
+	return AudioModel.findOne({ "id": id }).exec()
+	.then((info) => {
+		var aud = null;
+		if (info) {
+			aud = gfs.createReadStream({ filename: id });
+		}
+		return aud;
+	});
+};
+
+// MUTATOR
+exports.updateAudioTitle = (id, title) => { // set metadata
+	return AudioModel.findOne({ "id": id }).exec()
+	.then((info) => {
+		if (info === null) {
+			return null;
+		}
+		info.title = title;
+		return info.save()
+		.then((data) => {
+			console.log('saved ', data);
+		});
+	});
+};
+
+exports.audioSave = (audios) => { // set audio stream and metadata
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			if (null !== gfs) {
+				resolve();
+			}
+		}, 1000);
+	})
+	.then(() => {
+		return Promise.all(audios.map((aud) => {
+			if (null === aud || 
+				!(aud instanceof AudioSchema) ||
+				null == aud.audio) {
+				return null;
+			}
+
+			// save to database
+			var writeStream = gfs.createWriteStream({ filename: aud.id });
+			aud.audio.pipe(writeStream);
+			
+			var instance = new AudioModel({
+				'id': aud.id,
+				'source': aud.source,
+				'title': aud.title
+			});
+			
+			return new Promise((resolve, reject) => {
+				writeStream
+				.on('close', resolve)
+				.on('error', reject);
+			})
+			.then(() => {
+				return instance.save();
+			})
+			.then((data) => {
+				console.log(aud.id, " saved");
+			});
+		}));
+	})
+	.then(() => {
+		return audios.map((aud) => aud.id);
+	});
+};
+
+// >>>> POPULAR MODEL <<<<
+// ACCESSOR
+exports.popularQuery = () => {
+	var today = Math.floor(Date.now() / 1000);
+	return PopularModel.findOne({ "date": today }).exec()
+	.then((data) => {
+		var ids = []
+		if (data) {
+			ids = data.ids;
+		}
+		return ids;
+	});
+};
+
+// MUTATOR
+exports.popularSave = (ids) => {
+	console.log('saving to popular')
+    var today = Math.floor(Date.now() / 1000);
+    return new PopularModel({
+        'date': today,
+        'ids': ids
+    }).save();
+};
---
+++ testDb.js
@@ -0,0 +1,151 @@
+const chai = require('chai');
+const clean = require('mongo-clean');
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+const stream = require('fake-stream');
+
+// connect mongoose to mongo then get service
+const db = require('./index');
+const AudioSchema = require('./_schemas/audio_schema');
+
+const expect = chai.expect; // we are using the "expect" style of Chai
+const mongoURL = require('./connect_mongo').url;
+
+function sampleAudios () {
+	var length = 5 + Math.round(Math.random() * 13);
+	var sample = []
+	for (var i = 0; i < length; i++) {
+		var mockedStream = new stream();
+		sample.push(new AudioSchema({
+			"id": uuidv1(),
+			"source": ".test",
+			"title": uuidv1(),
+			"audio": mockedStream,
+		}));
+	}
+	return sample;
+};
+
+// behavior when database is empty
+describe('Audio Database (When Empty)', function() {
+	it('audioSave should return a list of ids', 
+	function(done) {
+		this.timeout(10000);
+
+		var testAudios = sampleAudios();
+		var nulIdx = Math.floor(Math.random() * testAudios.length);
+		testAudios[nulIdx].audio = null;
+		db.audioSave(testAudios)
+		.then((ids) => {
+			expect(ids).to.be.instanceof(Array);
+			expect(ids.length).to.equal(testAudios.length);
+			let idSet = new Set(ids);
+			expect(testAudios.every((amodel) => idSet.has(amodel.id))).to.be.true;
+			
+			clean(mongoURL, function (err, db) {
+				done();
+			});
+		})
+		.catch(done);
+	});
+
+	it('audioQuery on id should return null', 
+	function(done) {
+		var testAudios = sampleAudios();
+		var testIds = testAudios.map((amodel) => amodel.id);
+		db.audioQuery({ "id": { $in: testIds } })
+		.then((infos) => {
+			expect(infos.length).to.equal(0);
+		
+			done();
+		})
+		.catch(done);
+	});
+	
+	it('getAudio should return null',
+	function(done) {
+		var randId = uuidv1();
+		db.getAudio(randId)
+		.then((strm) => {
+			expect(strm).to.be.null;
+			
+			done();
+		})
+		.catch(done);
+	});
+    
+    it('popularQuery should return empty ids',
+    function(done) {
+        db.popularQuery()
+        .then((ids) => {
+			expect(ids.length).to.equal(0);
+			
+			done();
+        })
+		.catch(done);
+    });
+});
+
+// behavior when database already has an entry
+describe('Audio Database (With An Entry)', function() {
+    var savedIds;
+	before(function(done) {
+		this.timeout(10000);
+
+		var testAudios = sampleAudios();
+		db.audioSave(testAudios)
+		.then((ids) => {
+            savedIds = ids;
+            return db.popularSave(ids);
+        })
+        .then(() => {
+			done();
+		})
+		.catch(done);
+	});
+
+	it('audioQuery on id should return null', 
+	function(done) {
+		db.audioQuery({ "id": { $in: savedIds } })
+		.then((infos) => {
+			expect(infos.length).to.equal(savedIds.length);
+			var testIdSet = new Set(savedIds);
+			expect(infos.every((info) => testIdSet.has(info.id))).to.be.true;
+		
+			done();
+		})
+		.catch(done);
+	});
+	
+	it('getAudio should return not null',
+	function(done) {
+		db.getAudio(savedIds[0])
+		.then((strm) => {
+			expect(strm).to.not.be.null;
+			
+			done();
+		})
+		.catch(done);
+	});
+    
+    it('popularQuery should return saved ids',
+    function(done) {
+        db.popularQuery()
+        .then((ids) => {
+            let n = ids.length;
+            expect(n).to.equal(savedIds.length);
+            for (let i = 0; i < n; i++) {
+                expect(ids[i]).to.equal(savedIds[i]);
+			}
+			
+			done();
+        })
+		.catch(done);
+    });
+	
+	after(function(done) {
+		clean(mongoURL, function (err, db) {
+			done();
+		});
+	});
+});
